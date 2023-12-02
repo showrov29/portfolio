67c784caef3591179872e8de4dadf52d3305d0d9
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2023-12-02 15:30:01 +0600

    menu button click sound

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 import { Fade } from "react-awesome-reveal";
 import SoundBar from "./SoundBar/SoundBar";
 // import SoundButton from "./SoundButton/SoundButton";
-
+import { useEffect, useRef } from "react";
 const Header = ({ children }) => {
+	const audioRef = useRef(null);
+
 	return (
 		<nav className="w-full fixed top-0 py-8 z-50 select-none bg-gradient-to-b from-gray-dark-5 shadow-gray-dark-5 transition-all duration-300">
 			<Fade>
@@ -24,6 +26,10 @@ const Header = ({ children }) => {
 							className="checkbox-toggle link absolute top-0 right-0 w-6 h-6 opacity-0"
 							type="checkbox"
 							aria-label="menu"
+							onChange={(e) => {
+								audioRef.current.pause();
+								audioRef.current.play();
+							}}
 						/>
 						<div className="hamburger absolute top-2 right-0 w-6 h-6 flex items-center justify-center">
 							<div className="relative flex-none w-full bg-white duration-300 flex items-center justify-center"></div>
@@ -32,6 +38,8 @@ const Header = ({ children }) => {
 					</div>
 				</div>
 			</Fade>
+
+			<audio ref={audioRef} src="/sounds/menuClick.mp3" preload="auto" />
 		</nav>
 	);
 };
